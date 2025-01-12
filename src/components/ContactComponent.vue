1ab093e6cad1cffc1dc5f94752aff8ed7909f779
AMirza7 <afzal1226@gmail.com>
2025-01-12 07:27:21 -0500

    updated contact submit

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -10,91 +10,46 @@
             between 9:00 a.m. and 8:00 p.m. ET, Monday through Friday
           </p>
         </div>
-        <div class="contact_inner">
-          <div class="left wow fadeInLeft" data-wow-duration="1s">
-            <ul>
-              <li>
-                <div class="list_inner">
-                  <div class="icon orangeBackground">
-                    <i class="icon-location orangeText"></i>
-                  </div>
-                  <div class="short">
-                    <h3>Address</h3>
-                    <span>20, Somewhere in world</span>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="list_inner">
-                  <div class="icon greenBackground">
-                    <i class="icon-mail-1 greenText"></i>
-                  </div>
-                  <div class="short">
-                    <h3>Email</h3>
-                    <span><a href="#">hello@dizme.com</a></span>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="list_inner">
-                  <div class="icon purpleBackground">
-                    <i class="icon-phone purpleText"></i>
-                  </div>
-                  <div class="short">
-                    <h3>Phone</h3>
-                    <span>+123 456 7890</span>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <div class="right wow fadeInRight" data-wow-duration="1s">
+          <br>
+          <br>     
             <div class="fields">
-              <form
-                action="/"
-                method="post"
-                class="contact_form"
-                id="contact_form"
-                autocomplete="off"
-              >
-                <div
-                  class="returnmessage"
-                  data-success="Your message has been received, We will contact you soon."
-                ></div>
-                <div class="empty_notice">
-                  <span>Please Fill Required Fields</span>
+  <form
+    action="https://formspree.io/f/xvggdyqv"
+    method="POST"
+    class="contact_form"
+    id="contact_form"
+    autocomplete="off"
+  >
+    <div class="input_list">
+      <ul>
+        <li>
+          <input id="name" name="name" type="text" placeholder="Your Name" required />
+        </li>
+        <li>
+          <input id="email" name="email" type="email" placeholder="Your Email" required />
+        </li>
+        <li>
+          <input id="phone" name="phone" type="tel" placeholder="Your Phone" required/>
+        </li>
+        <li>
+          <input id="subject" name="subject" type="text" placeholder="Subject" required/>
+        </li>
+      </ul>
+    </div>
+    <div class="message_area">
+      <textarea
+        id="message"
+        name="message"
+        placeholder="Write your message here"
+        required
+      ></textarea>
+    </div>
+                  <div class="dizme_tm_button">
+                  <button class="button" id="send_message" style=" justify-content: center; align-items: center;" ><span>Submit Now</span></button>
                 </div>
-                <div class="input_list">
-                  <ul>
-                    <li>
-                      <input id="name" type="text" placeholder="Your Name" />
-                    </li>
-                    <li>
-                      <input id="email" type="text" placeholder="Your Email" />
-                    </li>
-                    <li>
-                      <input
-                        id="phone"
-                        type="number"
-                        placeholder="Your Phone"
-                      />
-                    </li>
-                    <li>
-                      <input id="subject" type="text" placeholder="Subject" />
-                    </li>
-                  </ul>
-                </div>
-                <div class="message_area">
-                  <textarea
-                    id="message"
-                    placeholder="Write your message here"
-                  ></textarea>
-                </div>
-                <div class="dizme_tm_button">
-                  <a id="send_message" href="#"><span>Submit Now</span></a>
-                </div>
-              </form>
-            </div>
+  </form>
+</div>
+
           </div>
           <div class="brush_2 wow fadeInRight" data-wow-duration="1s">
             <img src="img/brushes/contact/2.png" alt="" />
@@ -125,8 +80,6 @@
       <div class="brush_1 wow fadeInLeft" data-wow-duration="1s">
         <img src="img/brushes/contact/1.png" alt="" />
       </div>
-    </div>
-  </div>
 </template>
 
 <script>
